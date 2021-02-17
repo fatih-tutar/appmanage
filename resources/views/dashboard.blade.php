416674f67dc9cccd754b919e20c995f7b198b509
fatih-tutar <fatihtutar93@gmail.com>
2021-02-17 17:30:35 +0300

    Jetstream kurulumu yapıldı Bootstrap kurulumu yapıldı Jetstream profil foto yapılandırıldı Layout yapısı düzenlendi

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <x-slot name="header">Anasayfa</x-slot>
+
+    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <x-jet-welcome />
+    </div>
+</x-app-layout>
